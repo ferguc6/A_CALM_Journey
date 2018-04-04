0b9463501d41d723ab5188f38ae2f37f9269a61a
ferguc6@mcmaster.ca <fergusoncolin@DESKTOP-AU212J4.localdomain>
2018-04-04 16:49:55 -0400

    Created Driver File and Dice Roll Header

---
+++ Calm_Journey.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <Windows>
+
+using namespace std;
+
+int main(){
+  string s;
+  cout << "Applsauce" << endl;
+  cin >> s;
+  cin.get();
+  return 0;
+}
